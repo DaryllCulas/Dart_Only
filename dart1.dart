a2d3286cda76e89f870010368f069519801af27e
DaryllCulas <culasdaryll99@gmail.com>
2023-10-25 03:31:57 +0800

    User inputs in every data types

--- dart1.dart
+++ dart1.dart
@@ -1,7 +1,20 @@
 import 'dart:io';
 
 void main() {
-  print("Enter number:");
-  int? num = int.parse(stdin.readLineSync()!);
-  print("The entered number is $num");
+  // String User input
+  print("Enter your name: ");
+  String? name = stdin.readLineSync();
+
+  //Integer User input
+  print("Enter integer number: ");
+  int? integerNumber = int.parse(stdin.readLineSync()!);
+
+  //Floating User input
+  print("Enter floating number:");
+  double? doubleNumber = double.parse(stdin.readLineSync()!);
+
+  print("\n");
+  print("Your entered name is $name");
+  print("Your entered integer number/s is $integerNumber");
+  print("Your entered double number/s is $doubleNumber");
 }
