0f32f60b79d260f4d79f753b0c5b39cca18b5f94
DaryllCulas <culasdaryll99@gmail.com>
2023-10-25 04:06:06 +0800

    Escape strings

---
+++ dart2.dart
@@ -0,0 +1,8 @@
+// Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+
+void main() {
+  String name = 'Daryll';
+
+  print("Hello i am \"$name\"");
+  print("Hello I am \'$name\'");
+}
