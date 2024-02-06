54867d4a8c942cbead57f14ea5f6b2267b9ff30c
DaryllCulas <culasdaryll99@gmail.com>
2024-02-07 02:39:12 +0800

    Calculating Employee Salary

--- dart12.dart
+++ dart12.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 void main() {
-  givingValueToAddFunction();
+  employeeSalaryCalculation();
 }
 
 /*Providing default on positional parameter in function */
@@ -41,6 +43,7 @@ void inforVersion4() {
   printInfoVersion4("Daryll", 34, "Programmer");
 }
 
+// Parameter and no return type
 void simpleAddition(num1, num2) {
   int sum = num1 + num2;
   print("The sum is $sum");
@@ -56,4 +59,76 @@ void givingValueToAddFunction() {
   int num2 = 34;
 
   add(num1, num2);
+}
+
+// No parameter and return type
+
+void guessingAge() {
+  int personAge = 17;
+
+  if (personAge >= voterAge()) {
+    print("The person is eligible to vote");
+  } else {
+    print("The person is not eligible to vote");
+  }
+}
+
+int voterAge() {
+  return 18;
+}
+
+// Parameter and return type
+
+void calculatingPayroll() {
+  double rate = 80;
+  double hours = 45;
+  double bonus = 1.5;
+
+  double payrollResult = payrollSystem(rate, hours, bonus);
+  print("Your total salary is $payrollResult");
+}
+
+double payrollSystem(
+    double calculatedRate, double calculatedHours, double calculatedBonus) {
+  double calculating = calculatedRate * calculatedHours * calculatedBonus;
+
+  return calculating;
+}
+
+// Parameter and return type version 2
+
+void employeeSalaryCalculation() {
+  print("Enter Hourly rate:");
+  double? hourlyRate = double.parse(stdin.readLineSync()!);
+
+  print("Enter number of hours worked");
+  int? hoursWorked = int.parse(stdin.readLineSync()!);
+
+  print("Enter government contribution:");
+  double? govContribution = double.parse(stdin.readLineSync()!);
+
+  double tax = 0.10;
+
+  double payrollResult =
+      employeeSalaryBasis(hourlyRate, hoursWorked, govContribution, tax);
+
+  print("Your Salary is $payrollResult");
+  print("Your Contribution: $govContribution");
+  print("Your number of hours worked: $hoursWorked");
+}
+
+double employeeSalaryBasis(
+    double calculatedHourlyRate,
+    int calculatedHoursWorked,
+    double calculatedGovContribution,
+    double calculatedTax) {
+  double calculatingEmployeeSalary =
+      calculatedHourlyRate * calculatedHoursWorked;
+
+  double deductedByContribution =
+      calculatingEmployeeSalary - calculatedGovContribution;
+
+  double officialEmployeeSalary = deductedByContribution * calculatedTax;
+
+  return officialEmployeeSalary;
 }
