41708a33aada3c08c5e0ae900453510baddf7ace
DaryllCulas <culasdaryll99@gmail.com>
2024-02-09 01:33:10 +0800

    Add dart file

--- dart13.dart
+++ dart13.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  quizNumberFour();
+  quizNumberNine();
 }
 
 /*****************************************/
@@ -76,4 +76,75 @@ String generateRandomPassword(int length) {
     password += validCharacters[randomIndex];
   }
   return password;
+}
+
+/*****************************************/
+
+void quizNumberFive() {
+  print("Enter radius");
+  double? radix = double.parse(stdin.readLineSync()!);
+
+  double areaCircleComputation = areaOfCircle(radix);
+
+  print("The area of circle is $areaCircleComputation");
+}
+
+areaOfCircle(radix2) {
+  double radius = radix2 * radix2;
+
+  const pi = 3.14;
+
+  double areaOfCircle = radius * pi;
+  return areaOfCircle;
+}
+
+/*****************************************/
+
+void quizNumberSix() {
+  print("Enter String:");
+  String? str = stdin.readLineSync();
+
+  String reversedString = inputReversedStr(str);
+
+  print(">>> $reversedString");
+}
+
+String inputReversedStr(input) {
+  String reverse = "";
+
+  for (int i = input.length - 1; i >= 0; i--) {
+    reverse += input[i];
+  }
+  return reverse;
+}
+
+void quizNumberSeven() {
+  print("Enter number:");
+  int? number = int.parse(stdin.readLineSync()!);
+
+  int cubeOfNumber = powerOf(number);
+
+  print("The power of $number is $cubeOfNumber");
+}
+
+int powerOf(num) {
+  return num * num * num;
+}
+
+void quizNumberEight() {
+  print("Enter a number");
+  int? num = int.parse(stdin.readLineSync()!);
+
+  String isEvenOrOdd = OddOrEven(num);
+
+  print(">>> $num is an $isEvenOrOdd");
+}
+
+String OddOrEven(number) {
+  return number % 2 == 0 ? "This number is even" : "This number is odd";
+}
+
+void quizNumberNine() {
+  List<String> names = ["Raj", "John", "Rocky"];
+  print(names.first);
 }
---
+++ dart14.dart
@@ -0,0 +1,17 @@
+void main() {
+  growableLength();
+}
+
+/******************************************/
+void fixLength() {
+  var list = List<String>.filled(5, "Curry");
+
+  print(list);
+}
+
+/******************************************/
+
+void growableLength() {
+  var list1 = [210, 21, 22, 33, 44, 55];
+  print(list1);
+}
